5a12269083882b2392cfa318e153596be36f2e53
Eric Woodruff <Eric@EWoodruff.us>
2017-12-29 13:44:16 -0800

    Documentation project update

--- MasterBuild.bat
+++ MasterBuild.bat
@@ -1,11 +1,15 @@
 @ECHO OFF
 CLS
 
-"%WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild.exe" /nologo /v:m /m "Source\EWSListControls.sln" /t:Clean;Build "/p:Configuration=Release;Platform=Any CPU"
+REM Use the earliest version of MSBuild available
+IF EXIST "%ProgramFiles(x86)%\MSBuild\14.0" SET "MSBUILD=%ProgramFiles(x86)%\MSBuild\14.0\bin\MSBuild.exe"
+IF EXIST "%ProgramFiles(x86)%\MSBuild\12.0" SET "MSBUILD=%ProgramFiles(x86)%\MSBuild\12.0\bin\MSBuild.exe"
 
-"%WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild.exe" /nologo /v:m /m "Demos\ListControlDemo.sln" /t:Clean;Build "/p:Configuration=Release;Platform=x86"
+"%MSBUILD%" /nologo /v:m /m "Source\EWSListControls.sln" /t:Clean;Build "/p:Configuration=Release;Platform=Any CPU"
 
-IF NOT "%SHFBROOT%"=="" "%WINDIR%\Microsoft.Net\Framework\v4.0.30319\msbuild.exe" /nologo /v:m "Doc\EWSoftwareListControls.sln" /t:Clean;Build "/p:Configuration=Release;Platform=Any CPU"
+"%MSBUILD%" /nologo /v:m /m "Demos\ListControlDemo.sln" /t:Clean;Build "/p:Configuration=Release;Platform=x86"
+
+IF NOT "%SHFBROOT%"=="" "%MSBUILD%" /nologo /v:m "Doc\EWSoftwareListControls.sln" /t:Clean;Build "/p:Configuration=Release;Platform=Any CPU"
 
 IF "%SHFBROOT%"=="" ECHO **** Sandcastle help file builder not installed.  Skipping help build. ****
 
