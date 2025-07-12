15d6fd1267b4eaf58c085108e4a0c2a3d8dd27b4
EWSoftware <Eric@EWoodruff.us>
2025-07-11 18:21:53 -0700

    Minor update
    Set minimum version to .NET 4.8

--- MasterBuild.bat
+++ MasterBuild.bat
@@ -5,7 +5,7 @@ IF EXIST "%ProgramFiles%\Microsoft Visual Studio\2022\Community\MSBuild\Current"
 IF EXIST "%ProgramFiles%\Microsoft Visual Studio\2022\Professional\MSBuild\Current" SET "MSBUILD=%ProgramFiles%\Microsoft Visual Studio\2022\Professional\MSBuild\Current\bin\MSBuild.exe"
 IF EXIST "%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current" SET "MSBUILD=%ProgramFiles%\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\bin\MSBuild.exe"
 
-"%MSBUILD%" /nologo /v:m /m "Source\EWSListControls.sln" /t:Clean;Build "/p:Configuration=Release;Platform=Any CPU"
+"%MSBUILD%" /nologo /v:m /m "Source\EWSListControls.sln" /t:Clean;Restore;Build "/p:Configuration=Release;Platform=Any CPU"
 
 COPY Source\bin\Release\*.nupkg .\Deployment
 
